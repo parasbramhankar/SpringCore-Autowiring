640af15542dce8b0a313d194cb3364d189a0677b
parasbramhankar <parasbramhankar.in@gmail.com>
2025-11-21 14:29:10 +0530

    commit

--- src/main/java/org/example/Apple.java
+++ src/main/java/org/example/Apple.java
@@ -1,6 +1,6 @@
 package org.example;
 
-public class Apple implements Fruit{
+public class Apple implements Fruit1{
     public boolean isEatable(){
         System.out.println("This is an Apple");
         return false;
