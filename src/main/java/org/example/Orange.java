18b79114cd5ebc55de8edf2725e3a4b1df1f0ab0
parasbramhankar <parasbramhankar.in@gmail.com>
2025-11-21 14:29:17 +0530

    commit

--- src/main/java/org/example/Orange.java
+++ src/main/java/org/example/Orange.java
@@ -1,6 +1,6 @@
 package org.example;
 
-public class Orange implements Fruit{
+public class Orange implements Fruit1{
 
     public boolean isEatable(){
         System.out.println("This is an Orange");
