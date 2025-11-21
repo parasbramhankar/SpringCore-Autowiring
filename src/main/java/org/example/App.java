61c5eecebd4e2c9040c37837e36fffd0a0728237
parasbramhankar <parasbramhankar.in@gmail.com>
2025-11-21 14:29:06 +0530

    commit

--- src/main/java/org/example/App.java
+++ src/main/java/org/example/App.java
@@ -11,9 +11,20 @@ public class App
 {
     public static void main( String[] args )
     {
-        ApplicationContext context=new ClassPathXmlApplicationContext("bean.xml");
-        EatableFruit eat=context.getBean(EatableFruit.class);
-        eat.eatableFruit();
+//        ApplicationContext context=new ClassPathXmlApplicationContext("bean.xml");
+//        EatableFruit eat=context.getBean(EatableFruit.class);
+//        eat.eatableFruit();
+
+                ApplicationContext context = new ClassPathXmlApplicationContext("Bean.xml");
+
+                // First bean (Apple)
+                EatableFruit appleObj = context.getBean("eat", EatableFruit.class);
+                appleObj.eatableFruit();
+
+                // Second bean (Orange)
+//                EatableFruit orangeObj = context.getBean("eatable", EatableFruit.class);
+//                orangeObj.eatableFruit();
+
 
     }
 }
